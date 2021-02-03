a27f76e964e33c312734162e99f45780313b5f23
MaIsabelSolano <misolanob@gmail.com>
2021-02-03 17:17:33 -0600

    Initial commit

---
+++ src/CalculadoraGeneral.java
@@ -0,0 +1,9 @@
+public interface CalculadoraGeneral {
+    String Calculo (String expresion);
+    /**
+     * Se encarga de hacer los cálculos
+     * @param expresion del documento de texto
+     * @return String con respuesta numérica o indicando que no es correcta
+     */
+
+}
---
+++ src/Driver.java
@@ -0,0 +1,20 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Facultad de Ingeniería
+ * Dept. Ciencias de la Computación
+ * CC2003 Algoritmos y Estructuras de Datos
+ * @author Maria Isabel Solano 20504
+ * @version 1
+ */
+
+public class Driver {
+    public static void main (String[] args){
+
+
+        for (int i = 0; i < 10; i++){
+
+        }
+
+
+    }
+}
