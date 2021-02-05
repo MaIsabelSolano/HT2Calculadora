765c4b201a7fefe53bc6c23608d6ea6696d6ba0c
MaIsabelSolano <misolanob@gmail.com>
2021-02-05 00:02:59 -0600

    git commit tests

---
+++ Tests/StackVectorTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StackVectorTest {
+
+    @Test
+    void push() {
+    }
+
+    @Test
+    void pop() {
+    }
+
+    @Test
+    void peek() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void empty() {
+    }
+}
