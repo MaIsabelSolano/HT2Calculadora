9f860dbf1ac6acbcb46da2986c3d9563343c457c
Isabel Solano <60373842+MaIsabelSolano@users.noreply.github.com>
2021-02-05 20:35:03 -0600

    Update Stack.java

--- src/Stack.java
+++ src/Stack.java
@@ -1,37 +1,66 @@
 //Es la clase que se encontraba en canvas
 
-public interface Stack<E>
+import java.util.ArrayList;
+
+public class StackVector<E>
+        implements Stack<E>
 {
+    protected ArrayList<E> data;
 
     /**
-     *
+     * Construye el stack vacío
+     */
+    public StackVector()
+
+    {
+        data = new ArrayList<E>();
+    }
+
+    /**
+     * post:post: the value is added to the stack
+     *          will be popped next if no intervening push
      * @param item
      */
-    public void push(E item);
+    public void push(E item)
+    {
+        data.add(item);
+    }
 
     /**
-     * pre: stack not empty
-     * @return valor arriba del stack y lo elimina
+     * pre: pre: stack is not empty
+     * post: most recently pushed item is removed and returned
+     * @return  E
      */
-    public E pop();
+    public E pop()
+    {
+        return data.remove(size()-1);
+    }
 
     /**
-     * pre: stack not empty
-     * @return valor arriba del stack
+     * pre: stack is not empty
+     * post: top value (next to be popped) is returned
+     * @return E
      */
-    public E peek();
-
-
-    /** Determina si está vacío o no
-     *
-     * @return true si está vacío y false si no lo está
-     */
-    public boolean empty();
+    public E peek()
+    {
+        return data.get(size() - 1);
+    }
 
     /**
-     *
-     * @return int con la cantidad del elementos en el stack
+     * post: returns the number of elements in the stack
+     * @return int
      */
-    public int size();
+    public int size()
+    {
+        return data.size();
+    }
 
+    /**
+     * post: returns true if and only if the stack is empty
+     * @return boolean
+     */
+    public boolean empty()
+    {
+        return size() == 0;
+    }
 }
